1868f6d5d16b5e6e21a3095d0c93f728402e7ce3
rutkin.ryan@gmail.com <rutkin.ryan@gmail.com>
2023-04-05 09:13:04 -0500

    Exposed arrays of permitted Operators

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,7 @@
 export { checkCondition } from './checkCondition.js';
 export { Check, ValueCheck, ArrayCheck, InversionCheck } from './types/Check.type.js';
 export { Condition } from './types/Condition.type.js';
-export { ValueOperator, ArrayOperator, Operator, InversionOperator, LogicalOperator } from './types/Operator.type.js';
+export { ValueOperator, ArrayOperator, Operator, InversionOperator, LogicalOperator, VALUE_OPERATORS, ARRAY_OPERATORS, INVERSION_OPERATORS, LOGICAL_OPERATORS } from './types/Operator.type.js';
 export { InvalidCheckTarget } from './errors/InvalidCheckTarget.error.js';
 export { InvalidOperator } from './errors/InvalidOperator.error.js';
 export { isArrayCheck } from './guards/isArrayCheck.guard.js';
